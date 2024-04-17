99d2a02c45b846225b8da10377098cf273853f7a
Kelly Flippo <kellyflippo@169-231-207-112.wireless.ucsb.edu>
2024-04-17 15:30:14 -0700

    KF - finished zipcodequeryservice and wrote zipcodequeryservicetests

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/EarthquakeQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/EarthquakeQueryService.java
@@ -17,7 +17,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
-
+ 
 @Slf4j
 @Service
 public class EarthquakeQueryService {
