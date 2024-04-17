61eb55dd9820027524801f1558e48f830d4cccaf
Kriteen Shrestha <kshrestha@ucsb.edu>
2024-04-17 14:20:36 -0700

    ks - implement university controller and tests

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityController.java
@@ -2,8 +2,38 @@ package edu.ucsb.cs156.spring.backenddemo.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.ucsb.cs156.spring.backenddemo.services.UniversityQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
+@Tag(name="University info from universities.hipolabs.com")
 @RestController
+@RequestMapping("/api/university")
 public class UniversityController {
     
+    ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    UniversityQueryService universityQueryService;
+
+    @Operation(summary = "Get a list of universities that match a given name")
+    @GetMapping("/get")
+    @Parameter(name="name", description="name to search, e.g. 'Harvard' or 'Stanford'", example="Stanford")
+
+    public ResponseEntity<String> getUniversities(String name) throws JsonProcessingException {
+        String result = universityQueryService.getJSON(name);
+        return ResponseEntity.ok().body(result);
+    }
+
 }
