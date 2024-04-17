e937b3f3157896f4e9025641aa787162f3d49766
Kai Maeda <kaimaeda@ucsb.edu>
2024-04-16 18:44:26 -0700

    km - tested compilation in step 2.3

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/PublicHolidayQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/PublicHolidayQueryService.java
@@ -1,5 +1,7 @@
 package edu.ucsb.cs156.spring.backenddemo.services;
 
+import java.util.Map;
+
 import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +10,9 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
+import lombok.extern.slf4j.Slf4j;
+
+
 
 @Slf4j
 @Service
@@ -23,6 +28,8 @@ public class PublicHolidayQueryService {
     public static final String ENDPOINT = "https://date.nager.at/api/v2/publicholidays/{year}/{countryCode}";
 
     public String getJSON(String year, String countryCode) throws HttpClientErrorException {
+        log.info("year={}, countryCode={}", year, countryCode);
+        Map<String, String> uriVariables = Map.of("year", year, "countryCode", countryCode);
         return "";
     }
 
